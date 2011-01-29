bf033647d19db2be2152970e74761b6eeed6db6d
Michael Ryan <jasconius@gmail.com>
2011-01-28 23:24:49 -0500

    Added undo and reset buttons

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 image = display.newImage("Red1.png")
 
 clicks = {}
+lines = {}
 
 function tap(event)
 	
@@ -26,7 +27,9 @@ function tap(event)
 		local line = display.newLine(event.x, event.y, clicks[#clicks-1].x + image.x, clicks[#clicks-1].y + image.y)
 		line:setColor( 255, 255, 255, 255 )
 		line.width = 2
-	
+		
+		table.insert(lines, line)
+		
 	end
 	
 	print(log)
@@ -43,3 +46,43 @@ myRectangle:setStrokeColor(180, 180, 180)
 
 image:addEventListener("tap", tap)
 image:toFront()
+
+function undo(event)
+	
+	if #clicks > 0 then
+	
+		table.remove(clicks, #clicks)
+	
+		if #lines > 0 then
+	
+			lines[#lines]:removeSelf()
+	
+			table.remove(lines, #lines)
+	
+		end
+		
+	end
+	
+end
+
+local btnUndo = display.newImage("btn_undo.png")
+btnUndo.x = 220
+btnUndo.y = 80
+btnUndo:addEventListener("tap", undo)
+
+function reset(event)
+	
+	clicks = {}
+	
+	for i = 1, #lines do
+		lines[i]:removeSelf()
+	end
+	
+	lines = {}
+	
+end
+
+local btnReset = display.newImage("btn_reset.png")
+btnReset.x = 120
+btnReset.y = 80
+btnReset:addEventListener("tap", reset)
